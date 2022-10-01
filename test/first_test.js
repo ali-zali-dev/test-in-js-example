19e4ad11d90f933c71a38b831d17affc4ddd116e
Ali Zali <ali.zali.dev@gmail.com>
2022-10-01 20:29:44 +0330

    feat: first test

---
+++ test/first_test.js
@@ -0,0 +1,9 @@
+describe('app', function(){
+    it('first test',function(){
+        // throw Error('error')
+    })
+
+    it('second test',function(){
+        // throw Error('error')
+    })
+})
---
+++ test/second_test.js
@@ -0,0 +1,9 @@
+describe('app 2', function(){
+    it('first test',function(){
+        // throw Error('error')
+    })
+
+    it('second test',function(){
+        // throw Error('error')
+    })
+})
