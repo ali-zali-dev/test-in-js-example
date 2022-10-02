1391357fee2f6adce5f43eacc10bc2735683204f
Ali Zali <ali.zali.dev@gmail.com>
2022-10-02 20:09:22 +0330

    feat: add api article test

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ let config = require('config');
 console.log(config);
 const port = 3000
 
+const routes = require('./app/routes/index')
+
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DBHost , { useNewUrlParser : true});
 
@@ -14,6 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type : 'application/json'}));
+app.use(routes)
 
 
 app.listen(port , () => console.log(`listening on port ${port}`));
